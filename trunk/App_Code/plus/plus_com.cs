8c9949e9cbfa396385013f479a52c76b3bb58919
cainiao1v1 <cainiao1v1@fd8d2ccb-88aa-298f-6cce-050947a24b94>
2011-01-14 10:36:06 +0000

    plus

---
+++ trunk/App_Code/plus/plus_com.cs
@@ -0,0 +1,30 @@
+﻿namespace jtbc.plus
+{
+    using System;
+    using System.Drawing;
+    using System.Drawing.Drawing2D;
+    using System.Drawing.Imaging;
+    using System.IO;
+    using System.Net;
+    using System.Net.Mail;
+    using System.Text;
+    using System.Web;
+    using jtbc;
+
+    public static class plus_com
+    {
+        public static string webAdminHead(string argKey)
+        {
+            string str = argKey;
+            return jt.creplace(jt.itake(string.Format("global.{0}:tpl_public.{1}", config.adminFolder, str), "tpl"));
+        }
+
+        public static string webAdminFoot(string argKey)
+        {
+            string str = argKey;
+            return jt.creplace(jt.itake(string.Format("global.{0}:tpl_public.{1}", config.adminFolder, str), "tpl"));
+        }
+    }
+}
+
+
--- trunk/admin2/common/codefile/default.cs
+++ trunk/admin2/common/codefile/default.cs
@@ -1,11 +1,12 @@
 ﻿using jtbc;
+using jtbc.plus;
 
 public partial class module : jpage
 {
     protected void Page_Load()
     {
         PageInit();
-        string tmpstr = jt.ireplace("default.default2", "tpl");
+        string tmpstr = plus_jt.ireplace("default.default", "tpl");
         PagePrint(tmpstr);
         PageClose();
     }
