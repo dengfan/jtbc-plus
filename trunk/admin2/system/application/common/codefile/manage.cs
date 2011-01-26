d791d1d5437331eafad74e4a78918e5b11d323dc
cainiao1v1 <cainiao1v1@fd8d2ccb-88aa-298f-6cce-050947a24b94>
2011-01-26 03:21:35 +0000

    缓存管理页面

--- trunk/admin2/system/application/common/codefile/manage.cs
+++ trunk/admin2/system/application/common/codefile/manage.cs
@@ -1,12 +1,88 @@
 using jtbc;
+using jtbc.plus;
 
-public partial class module: jpage
+public partial class module : jpage
 {
-  protected void Page_Load()
-  {
-    PageInit();
-    string tmpstr = jt.ireplace("manage.default", "tpl");
-    PagePrint(tmpstr);
-    PageClose();
-  }
+    private admin admin;
+
+    private string Module_Action_Delete()
+    {
+        string tstate = "200";
+        string tname = cls.getSafeString(request.querystring("name"));
+        if (!cls.isEmpty(tname))
+        {
+            tname = cls.getLRStr(tname, config.appName, "rightr");
+            application.remove(tname);
+        }
+        else application.removeall();
+        return tstate;
+    }
+
+    private string Module_Action()
+    {
+        string tmpstr = "";
+        string tAtype = cls.getString(request.querystring("atype"));
+        switch (tAtype)
+        {
+            case "delete":
+                tmpstr = Module_Action_Delete();
+                break;
+        }
+        return tmpstr;
+    }
+
+    private string Module_List()
+    {
+        string tmpstr = "";
+        string tmpastr, tmprstr, tmptstr;
+        string tkeyword = cls.getSafeString(request.querystring("keyword"));
+        tmpstr = jt.itake("manage.default2", "tpl");
+        tmprstr = "";
+        tmpastr = cls.ctemplate(ref tmpstr, "{@}");
+        foreach (string tAppName in application.getContents())
+        {
+            if (cls.isEmpty(tkeyword) || tAppName.IndexOf(tkeyword) >= 0)
+            {
+                tmptstr = tmpastr;
+                tmptstr = tmptstr.Replace("{$name}", encode.htmlencode(tAppName));
+                tmprstr += tmptstr;
+            }
+        }
+        tmpstr = tmpstr.Replace(config.jtbccinfo, tmprstr);
+        tmpstr = plus_jt.creplace(tmpstr);
+        //tmpstr = config.ajaxPreContent + tmpstr;
+        return tmpstr;
+    }
+
+    protected void Page_Load()
+    {
+        PageInit();
+        PageNoCache();
+
+        admin = new admin();
+        admin.Init();
+
+        if (admin.ckLogin())
+        {
+            string tmpstr = "";
+            string tType = cls.getString(request.querystring("type"));
+
+            switch (tType)
+            {
+                case "action":
+                    tmpstr = Module_Action();
+                    break;
+                case "list":
+                    tmpstr = Module_List();
+                    break;
+                default:
+                    tmpstr = Module_List();
+                    break;
+            }
+
+            PagePrint(tmpstr);
+        }
+
+        PageClose();
+    }
 }
