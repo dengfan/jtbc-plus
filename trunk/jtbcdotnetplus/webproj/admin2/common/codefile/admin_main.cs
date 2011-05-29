24f8530767918dead6c4fd8569b87578cfa33aef
cainiao1v1 <cainiao1v1@fd8d2ccb-88aa-298f-6cce-050947a24b94>
2011-05-29 08:00:24 +0000

    恢复到106版本

--- trunk/jtbcdotnetplus/webproj/admin2/common/codefile/admin_main.cs
+++ trunk/jtbcdotnetplus/webproj/admin2/common/codefile/admin_main.cs
@@ -1,10 +1,8 @@
 ﻿using jtbc;
-using jtbc.plus;
 
 public partial class module : jpage
 {
     private admin _admin;
-    private admin_plus _admin_plus;
 
     protected void Page_Load()
     {
@@ -15,13 +13,10 @@ public partial class module : jpage
         _admin.Init();
         _admin.adminPstate = "public";
 
-        _admin_plus = new admin_plus(_admin); //扩展admin类
-
         if (!_admin.ckLogin()) Response.Redirect("default.aspx");
 
         string tmpstr = "";
         tmpstr = jt.ireplace("main.admin_frame", "tpl");
-        tmpstr = tmpstr.Replace("{$leftmenu}", _admin_plus.getMenuHtml(cls.getActualRoute("./")));
 
         PagePrint(tmpstr);
         PageClose();
--- trunk/jtbcdotnetplus/webproj/admin2/common/images/js/global.js
+++ trunk/jtbcdotnetplus/webproj/admin2/common/images/js/global.js
@@ -3,9 +3,3 @@ j('.table1 tbody tr').hover(
     function () { j(this).addClass('bg1'); },
     function () { j(this).removeClass('bg1'); }
 );
-
-//隐藏右键菜单
-j('body').click(function () {
-    j('.contextmenu1', parent.document).hide(); //父框架页面，如左侧导航菜单的右键菜单
-    j('.contextmenu1').hide();
-});
